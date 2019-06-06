cb6168b7e52ac04aa55d01b3b30cfb6d818e71e9
Ed Noepel <ed@makerdao.com>
2019-06-06 16:30:20 -0400

    Updated path to setup-env.sh script, clean up submodules when updating

--- bin/deploy-mcd.sh
+++ bin/deploy-mcd.sh
@@ -8,7 +8,7 @@ export TESTNET_HOST="parity"
 export KEYSTORE_PATH=/testchain-data/keys/DevelopmentChain
 export ETH_FROM=0x00a329c0648769A73afAc7F9381E08FB43dBEA72
 
-. scripts/setup-env.sh
+. lib/setup-env.sh
 ./step-1-deploy
 ./step-2-deploy
 ./step-3-deploy
--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 git clone https://github.com/makerdao/testchain-dss-deployment-scripts.git src/deployment-scripts
-cd src/deployment-scripts
+pushd src/deployment-scripts
+git fetch --all --tags --prune
+git checkout tags/0.2.7
+git submodule deinit -f --all && dapp update
 git submodule update --init --recursive
